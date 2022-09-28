6d13724955994ce388d6677b156d96bd4d9549cd
eduardo <eduardo.salinas3498@gmail.com>
2022-09-27 22:36:50 -0500

    Ejercicio Hecho

--- src/main/java/pe/ineapp/ineappalumnoservice/Alumno/Alumno.java
+++ src/main/java/pe/ineapp/ineappalumnoservice/Alumno/Alumno.java
@@ -1,17 +1,16 @@
 package pe.ineapp.ineappalumnoservice.Alumno;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Alumno {
 
+    Long id;
     String nombre;
-
+    String apellido;
+    String dni;
+    String curso;
 
 }
--- src/main/java/pe/ineapp/ineappalumnoservice/Alumno/AlumnoController.java
+++ src/main/java/pe/ineapp/ineappalumnoservice/Alumno/AlumnoController.java
@@ -4,16 +4,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/alumno")
 public class AlumnoController {
 
     @GetMapping
     @RequestMapping("/getAll")
-    public String getAlumno(){
+    public List<Alumno> getAll(){
 
-        Alumno alumno = new Alumno("Eduardo");
+        Alumno alumno = Alumno.builder()
+                .id(1L)
+                .nombre("Eduardo")
+                .apellido("Salinas")
+                .dni("72879084")
+                .curso("Integracion")
+                .build();
 
-        return alumno.getNombre();
+        Alumno alumno2 = Alumno.builder()
+                .id(2L)
+                .nombre("Kevin")
+                .apellido("Moya")
+                .dni("72322544")
+                .curso("Mecanica I")
+                .build();
+
+        Alumno alumno3 = Alumno.builder()
+                .id(3L)
+                .nombre("Valeria")
+                .apellido("Pimentel")
+                .dni("70616287")
+                .curso("Fundamentos de Marketing")
+                .build();
+
+        return List.of(alumno, alumno2, alumno3);
     }
 }
